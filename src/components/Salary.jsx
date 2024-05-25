5e8ed4c4c0f13c4c24d2b4bf6da1033ef78f1c58
Smirnov Ivan <>
2024-05-25 22:04:13 +0300

    update Salary

--- src/components/Salary.jsx
+++ src/components/Salary.jsx
@@ -1,5 +1,5 @@
-import imageSrc1 from '../assets/rub.png';
-import imageSrc2 from '../assets/hh.png';
+import imageSrc1 from '../assets/rub.svg';
+import imageSrc2 from '../assets/hh.svg';
 import imageSrc3 from '../assets/pointer.svg';
 import imageSrc4 from '../assets/img.png';
 import imageSrc5 from '../assets/img2.png';
@@ -7,35 +7,41 @@ import imageSrc5 from '../assets/img2.png';
 export const Salary = () => {
     return (
         <div className='salary'>
-            <div className='salary-block-1'>
-                <img className='salary-icon' src={imageSrc1} alt='1'/>
-                <p className='salary-text'>
-                    Средняя зарплата копирайтера — <span>75 880 ₽</span>
-                </p>
-                <p className='salary-desk'>
-                    Для сравнения, средняя зарплата
-                    начинающего it-специалиста — 52 000 ₽
-                </p>
+            <div className='salary-blocks'>
+                <div className='salary-block-1'>
+                    <img className='salary-icon' src={imageSrc1} alt='1'/>
+                    <div className='salary-text'>
+                        Средняя зарплата копирайтера — <br/>
+                        <span>75 880 ₽</span>
+                    </div>
+                    <div className='salary-desk'>
+                        Для сравнения, средняя зарплата
+                        начинающего it-специалиста — <span>52 000 ₽</span>
+                    </div>
+                </div>
+
+                <div className='salary-block-2'>
+                    <img className='salary-icon' src={imageSrc2} alt='2'/>
+                    <p className='salary-text'>
+                        6773 компании<br/> ищут копирайтеров прямо сейчас
+                    </p>
+                </div>
+
+                <div className='salary-block-3'>
+                    <img className='pointer' src={imageSrc3} alt='3'/>
+                </div>
             </div>
 
-            <div className='salary-block-2'>
-                <img className='salary-icon' src={imageSrc2} alt='2'/>
-                <p className='salary-text'>
-                    6773 компании
-                    ищут копирайтеров
-                    прямо сейчас
-                </p>
-            </div>
+            <div className='salary-blocks'>
+                <div className='salary-block-3'>
 
-            <div className='salary-block-3'>
-                <img className='pointer' src={imageSrc3} alt='3'/>
-            </div>
-
-            <div className='salary-block-4'>
-                <img src={imageSrc4} alt='3'/>
-            </div>
-            <div className='salary-block-5'>
-                <img src={imageSrc5} alt='3'/>
+                </div>
+                <div className='salary-block-4'>
+                    <img src={imageSrc4} alt='3'/>
+                </div>
+                <div className='salary-block-5'>
+                    <img src={imageSrc5} alt='3'/>
+                </div>
             </div>
 
         </div>
