c153d32d8dff33ca629fce98877b4cb5c2bb9fd3
Smirnov Ivan <>
2024-05-26 14:36:45 +0300

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,36 +1,51 @@
-import React from "react";
+import React, {useEffect} from "react";
 import image1 from '../assets/GasProm.svg';
-import image2 from '../assets/author.svg';
+import image2 from '../assets/mask.svg';
 import image3 from '../assets/Cian.svg';
 import image4 from '../assets/Tinko.svg';
 import image5 from '../assets/alfa.svg';
+import arrow from "../assets/Arrow 1 (Stroke).svg";
+import starBlack from "../assets/star_black.svg";
 
 
 export const About = () => {
+
     return (
         <div className='main about'>
             <div className='title'>
                 Преподаватель
                 Марк
-                Николаев
+                <div style={{textAlign: "right"}}>
+                    <img src={arrow} alt='1'
+                         style={{
+                             height: 72,
+                             width: 72,
+                             display: "inline-block",
+                             position: "relative",
+                             top: '-142px',
+                         }}/>
+                    Николаев
+                </div>
             </div>
             <div className='main-blocks'>
                 <div className='blocks'>
                     <div className='block-m'>
-                        <div className='star'>*</div>
-                        <div className='text'>Главный редактор
-                            УК Газпромбанк,
-                            ex-редактор
-                            Тинькофф Банка
-                            и основатель
-                            рекламного агентства
+                        <div className="icon">
+                            <img src={starBlack} className='star'/>
+                        </div>
+                        <div className='text'>Главный редактор<br/>
+                            УК Газпромбанк, <br/>
+                            ex-редактор <br/>
+                            Тинькофф Банка <br/>
+                            и основатель<br/>
+                            рекламного агентства<br/>
                             Crystall.Media
                         </div>
                     </div>
                     <div className='block-1'>
                         <img className='name' src={image1} alt='1'/>
                         <div className='block-title'>
-                            Газпромбанк
+                            Газпромбанк <br/>
                             Капитиал
                         </div>
                         <div className='text'>
@@ -43,9 +58,13 @@ export const About = () => {
                             рублей каждый месяц.
                         </div>
                     </div>
-                    <div className='block-author'>
-                        <img className='author' src={image2} alt='2'/>
+
+                    <div className='main-author'>
+                        <div className="block-author">
+                            <img className='author'  src={image2} alt='2'/>
+                        </div>
                     </div>
+
                 </div>
                 <div className='blocks'>
                     <div className='block-2'>
@@ -54,13 +73,13 @@ export const About = () => {
                             Циан
                         </div>
                         <div className='text'>
-                            Полгода отвечал за выпуск статей
-                            на Яндекс.Дзен. Собирал и
-                            контролировал команду авторов,
-                            согласовывал статьи с редакцией.
-                            Вели блог на тему недвижимости:
-                            рассказывали, как купить новую
-                            квартиру в ипотеку или продать
+                            Полгода отвечал за выпуск статей <br/>
+                            на Яндекс.Дзен. Собирал и <br/>
+                            контролировал команду авторов, <br/>
+                            согласовывал статьи с редакцией. <br/>
+                            Вели блог на тему недвижимости: <br/>
+                            рассказывали, как купить новую <br/>
+                            квартиру в ипотеку или продать <br/>
                             старую дороже рынка.
                         </div>
                     </div>
