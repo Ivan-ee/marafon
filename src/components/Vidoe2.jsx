9230823494a956141fb988024a10952cd0927018
Smirnov Ivan <>
2024-05-26 11:44:44 +0300

    update

--- src/components/Vidoe2.jsx
+++ src/components/Vidoe2.jsx
@@ -1,21 +1,31 @@
+import VideoModal from "./YouTube.jsx";
+import prev1 from "../assets/123.png";
+import React from "react";
+import starBlack1 from "../assets/star_black.svg";
+import telegram from "../assets/telegramm.svg";
+
 export const Video2 = () => {
     return (
-        <div className='video2'>
-            <div className='text'>
-                <div className='icon'>*</div>
-                Смотрите бесплатный
-                видеоурок в Телеграме
-                о том, как выйти
-                на доход от 50 тысяч
-                рублей в месяц.
+
+        <div className='video2-block'>
+            <div className='video2'>
+                <div className='text'>
+                    <div className="icon">
+                        <img src={starBlack1} className="star" alt="star"/>
+                    </div>
+                    Смотрите бесплатный
+                    видеоурок в Телеграме <br/>
+                    о том, как выйти <br/>
+                    на доход от 50 тысяч <br/>
+                    рублей в месяц.
+                </div>
+                <div className='video-wrapper'>
+                    <VideoModal videoId="LcGh0g0Aycs" imageSrc={prev1}/>
+                </div>
             </div>
-            <div className='video-wrapper'>
-                <iframe width="560" height="315"
-                        src="https://www.youtube-nocookie.com/embed/JZEB8BSLm4I?si=ftHkOiMlhIG63ccs"
-                        title="YouTube video player" frameBorder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-            </div>
+            <button className='btn-video'>
+                Получить урок <img src={telegram} className="telegram" alt="telegram"/>
+            </button>
         </div>
     )
 }
