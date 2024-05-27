e704b9835cf232010e0e6450a8fe5516db777e46
Smirnov Ivan <>
2024-05-28 00:05:50 +0300

    upadate desk

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,34 +1,75 @@
-import React, {useEffect, useRef} from "react";
-import {Accordion, AccordionItem} from "@nextui-org/react";
+import React, {useEffect, useRef, useState} from "react";
 import image from '../assets/pol2.svg';
 import image2 from "../assets/galka.svg";
 import image3 from "../assets/Marathon layout (4).svg";
 import {Die} from "./Die.jsx";
+import arrowWhite from "../assets/up.svg";
+import arrowBlack from "../assets/down.svg";
 
 export const Description = () => {
-    useEffect(() => {
-        const svgElements = document.querySelectorAll('svg[aria-hidden="true"][focusable="false"][role="presentation"][viewBox="0 0 24 24"]');
 
-        svgElements.forEach(svg => {
-            const img = document.createElement('img');
-            img.src = image;
-            img.alt = 'Alternative text';
-            img.className = 'replaced-icon';
-            svg.parentNode.replaceChild(img, svg);
-        });
-
-        const elements = document.querySelectorAll('[aria-label]');
-        elements.forEach(element => {
-            element.classList.add('custom-p');
-        })
-
-        const titleElement = document.querySelector('.description .title');
-        const nextElement = titleElement.nextElementSibling;
-        if (nextElement) {
-            nextElement.style.padding = '0';
-        }
-
-    }, []);
+    const [accordionItemsDesc, setAccordionItemsDesc] = useState([
+        {
+            id: "accordion-d-1",
+            title:
+                "1 день",
+            content:
+                "Поймёте, как сделать так, чтобы человек прочитал ваш текст и \n" +
+                "правильно его понял. Разберёте четыре ошибки, которые совершают \n" +
+                "начинающие авторы. Узнаете, как выделиться среди конкурентов.",
+            t1: "Текстовый урок про сильный текст — цель и пользу для читателя",
+            t2: "Текстовый урок про запретные слова — что нельзя использовать и почему",
+            t3: "Домашняя работа — 10 тестовых и практических заданий",
+            t4: "Вебинар от Марка",
+        },
+        {
+            id: "accordion-d-2",
+            title:
+                "2 день",
+            content:
+                "Научитесь писать простыми словами. Сможете редактировать тексты \n" +
+                "для личного блога, собственных или рабочих проектов — Инстаграма*, \n" +
+                "Дзена или любого диджитал медиа.",
+            t1: "2 текстовых урока про словестный мусор",
+            t2: "Домашнее задание — 8 тестовых и практических заданий",
+            t3: "Вебинар от Марка",
+        },
+        {
+            id: "accordion-d-3",
+            title:
+                "3 день",
+            content:
+                "Разберёте продвинутые инструменты редактуры — примеры и сценарии. \n" +
+                "Научитесь убеждать читателя в пользе вашего продукта и узнаете, \n" +
+                "за какие тексты готовы платить от 5 тысяч рублей.",
+            t1: "Текстовый урок про мир читателя и полезное действие",
+            t2: "Домашнее задание — 7 тестовых и практических заданий",
+            t3: "Вебинар от Марка",
+        },
+        {
+            id: "accordion-d-4",
+            title:
+                "День 4",
+            content:
+                "Научитесь работать с аудиторией — анализировать её и использовать \n" +
+                "это в работе. Напишете свой первый текст для портфолио, с которым \n" +
+                "сможете откликаться на удаленные вакансии.",
+            t1: "Текстовый урок — как писать контекст, призыв к действию и рассказывать о себе",
+            t2: "Домашнее задание — пост для личного блога",
+            t3: "Подробный разбор текста от редактора",
+        },
+        {
+            id: "accordion-d-5",
+            title:
+                "День 5",
+            content:
+                "Узнаете, как составлять резюме и откликаться даже без опыта работы. \n" +
+                "Получите пошаговую инструкцию, как взять первого клиента. Разберётесь \n" +
+                "в причинах синдрома самозванца, и поймете, как их преодолеть.",
+            t1: "Видеоурок от Марка",
+            t2: "2-часовой вебинар от приглашенного спикера",
+        },
+    ]);
 
     const descriptionRef = useRef(null);
 
@@ -44,114 +85,79 @@ export const Description = () => {
                 <div>👩🏼‍🏫</div>
                 <div>👩🏼‍🍼</div>
             </Die>
-            <Accordion variant="splitted">
-                <AccordionItem className='a-style' key="1" aria-label="Accordion 1" title="1 день">
-                    <div className='text-desk'>
-                        Поймёте, как сделать так, чтобы человек прочитал ваш текст и <br/>
-                        правильно его понял. Разберёте четыре ошибки, которые совершают <br/>
-                        начинающие авторы. Узнаете, как выделиться среди конкурентов.
-                    </div>
-                    <div className='select'>
-                        <div className='item'>
-                            <img src={image3} style={{marginTop: 5}} alt='2'/>
-                            <div>Текстовый урок про сильный текст — цель и пользу для читателя</div>
-                        </div>
-                        <div className='item'>
-                            <img src={image3} style={{marginTop: 5}} alt='2'/>
-                            <div>Текстовый урок про запретные слова — что нельзя использовать и почему</div>
-                        </div>
-                        <div className='item'>
-                            <img src={image3} style={{marginTop: 5}} alt='2'/>
-                            <div>Домашняя работа — 10 тестовых и практических заданий</div>
-                        </div>
-                        <div className='item'>
-                            <img src={image3} style={{marginTop: 5}} alt='2'/>
-                            <div>Вебинар от Марка</div>
-                        </div>
-                    </div>
-                </AccordionItem>
-                <AccordionItem className='a-style' key="2" aria-label="Accordion 2" title="2 день">
-                    <div className='text-desk'>
-                        Научитесь писать простыми словами. Сможете редактировать тексты <br/>
-                        для личного блога, собственных или рабочих проектов — Инстаграма*, <br/>
-                        Дзена или любого диджитал медиа.
-                    </div>
-                    <div className='select'>
-                        <div className='item'>
-                            <img src={image3} style={{marginTop: 5}} alt='2'/>
-                            <div>2 текстовых урока про словестный мусор</div>
-                        </div>
-                        <div className='item'>
-                            <img src={image3} style={{marginTop: 5}} alt='2'/>
-                            <div>Домашнее задание — 8 тестовых и практических заданий</div>
-                        </div>
-                        <div className='item'>
-                            <img src={image3} style={{marginTop: 5}} alt='2'/>
-                            <div>Вебинар от Марка</div>
-                        </div>
-                    </div>
-                </AccordionItem>
-                <AccordionItem className='a-style' key="3" aria-label="Accordion 3" title="3 день">
-                    <div className='text-desk'>
-                        Разберёте продвинутые инструменты редактуры — примеры и сценарии.
-                        Научитесь убеждать читателя в пользе вашего продукта и узнаете, <br/>
-                        за какие тексты готовы платить от 5 тысяч рублей.
-                    </div>
-                    <div className='select'>
-                        <div className='item'>
-                            <img src={image3} style={{marginTop: 5}} alt='2'/>
-                            <div>Текстовый урок про мир читателя и полезное действие</div>
-                        </div>
-                        <div className='item'>
-                            <img src={image3} style={{marginTop: 5}} alt='2'/>
-                            <div>Домашнее задание — 7 тестовых и практических заданий</div>
-                        </div>
-                        <div className='item'>
-                            <img src={image3} style={{marginTop: 5}} alt='2'/>
-                            <div>Вебинар от Марка</div>
-                        </div>
-                    </div>
-                </AccordionItem>
-                <AccordionItem className='a-style' key="4" aria-label="Accordion 4" title="4 день">
-                    <div className='text-desk'>
-                        Научитесь работать с аудиторией — анализировать её и использовать <br/>
-                        это в работе. Напишете свой первый текст для портфолио, с которым <br/>
-                        сможете откликаться на удаленные вакансии.
-                    </div>
-                    <div className='select'>
-                        <div className='item'>
-                            <img src={image3} style={{marginTop: 5}} alt='2'/>
-                            <div>Текстовый урок — как писать контекст, призыв к действию и рассказывать о себе</div>
-                        </div>
-                        <div className='item'>
-                            <img src={image3} style={{marginTop: 5}} alt='2'/>
-                            <div>Домашнее задание — пост для личного блога</div>
-                        </div>
-                        <div className='item'>
-                            <img src={image3} style={{marginTop: 5}} alt='2'/>
-                            <div>Подробный разбор текста от редактора</div>
-                        </div>
-                    </div>
-                </AccordionItem>
-                <AccordionItem className='a-style' key="5" aria-label="Accordion 5" title="5 день">
-                    <div className='text-desk'>
-                        Узнаете, как составлять резюме и откликаться даже без опыта работы.
-                        Получите пошаговую инструкцию, как взять первого клиента. Разберётесь
-                        в причинах синдрома самозванца, и поймете, как их преодолеть.
-                    </div>
-                    <div className='select'>
-                        <div className='item'>
-                            <img src={image3} style={{marginTop: 5}} alt='2'/>
-                            <div>Видеоурок от Марка</div>
-                        </div>
-                        <div className='item'>
-                            <img src={image3} style={{marginTop: 5}} alt='2'/>
-                            <div>2-часовой вебинар от приглашенного спикера</div>
-                        </div>
-                    </div>
-                </AccordionItem>
-            </Accordion>
+            <section>
+                <div className="accordion-description">
+                    {accordionItemsDesc.map((item, index) => (
+                        <AccordionItem
+                            key={item.id}
+                            item={item}
+                            index={index}
+                            accordionItems={accordionItemsDesc}
+                            setAccordionItems={setAccordionItemsDesc}
+                        />
+                    ))}
+                </div>
+            </section>
 
         </div>
     )
 }
+
+const AccordionItem = ({item, index, accordionItems, setAccordionItems}) => {
+    const arrowRefDesc = useRef(null);
+
+    useEffect(() => {
+        const button = document.getElementById(item.id);
+
+        const toggleAccordion = (event) => {
+            setAccordionItems(
+                accordionItems.map((accItem, accIndex) => ({
+                    ...accItem,
+                    ariaExpanded: accIndex === index ? !accItem.ariaExpanded : false,
+                }))
+            );
+        };
+
+        button.addEventListener("click", toggleAccordion);
+
+        return () => {
+            button.removeEventListener("click", toggleAccordion);
+        };
+    }, [item.id, index, accordionItems, setAccordionItems]);
+
+    return (
+        <div className='accordion-item'>
+            <button id={item.id} aria-expanded={item.ariaExpanded || false} className={item.buttonClass}>
+                <div className="accordion-title">{item.title}</div>
+                <div className="icon" aria-hidden="true">
+                    <img
+                        ref={arrowRefDesc}
+                        src={item.color === "white" ? arrowWhite : arrowBlack}
+                        alt="1"
+                        style={{transform: item.ariaExpanded ? "rotate(180deg)" : "rotate(0deg)"}}
+                    />
+                </div>
+            </button>
+            <div className="accordion-content">
+                <p>{item.content}</p>
+                <div className='select'>
+                    <div className='item'><img src={image3} style={{marginTop: 5}} alt='2'/>{item.t1}</div>
+                    <div className='item'><img src={image3} style={{marginTop: 5}} alt='2'/>{item.t2}</div>
+                    {item.t3 && (
+                        <div className='item'>
+                            <img src={image3} style={{marginTop: 5}} alt='2'/>
+                            {item.t3}
+                        </div>
+                    )}
+                    {item.t4 && (
+                        <div className='item'>
+                            <img src={image3} style={{marginTop: 5}} alt='2'/>
+                            {item.t4}
+                        </div>
+                    )}
+                </div>
+
+            </div>
+        </div>
+    );
+};
