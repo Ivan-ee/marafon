cfcf58498c64329bda83634c5e125d764bad7f2f
Smirnov Ivan <>
2024-05-30 10:37:29 +0300

    minor update

--- src/components/After.jsx
+++ src/components/After.jsx
@@ -1,11 +1,40 @@
 import {Block} from "./block.jsx";
 import starBlack from "../assets/star_black.svg";
-import React from "react";
+import React, {useEffect, useRef, useState} from "react";
 import image from "../assets/Arrow 1.svg";
 import arrow from "../assets/Arrow 1 Stroke (1).svg";
 import {Die} from "./Die.jsx";
 
 export const After = () => {
+
+    const currentDate = new Date();
+
+    const formattedDate = currentDate.toLocaleDateString('ru-RU', {
+        day: '2-digit',
+        month: '2-digit',
+    });
+
+    const [timeLeft, setTimeLeft] = useState(getTimeLeft());
+
+    function getTimeLeft() {
+        const now = new Date();
+        const endOfDay = new Date(now);
+        endOfDay.setHours(23, 59, 59, 999);
+        const diff = endOfDay - now;
+        const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+        const minutes = Math.floor((diff / 1000 / 60) % 60);
+        const seconds = Math.floor((diff / 1000) % 60);
+        return {hours, minutes, seconds};
+    }
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTimeLeft(getTimeLeft());
+        }, 1000);
+
+        return () => clearInterval(timer);
+    }, []);
+
     return (
         <div className='main after'>
             <div className='title'>
@@ -50,7 +79,7 @@ export const After = () => {
                             -88% до конца дня
                         </p>
                         <p className='timer'>
-                            12:53:23
+                            {`${timeLeft.hours.toString().padStart(2, '0')}:${timeLeft.minutes.toString().padStart(2, '0')}:${timeLeft.seconds.toString().padStart(2, '0')}`}
                         </p>
                         <div className='last-price'>
                             3990 ₽
