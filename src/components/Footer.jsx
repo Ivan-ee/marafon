5db0c1817a16eb5090f9a8fb8bdb606f45e9c0d2
Smirnov Ivan <>
2024-06-18 12:05:12 +0300

    update seo footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import useSmoothScroll from "./SmoothScroll.jsx";
-import React, { useRef } from "react";
-import { Button } from "./Button.jsx";
+import React, {useRef} from "react";
+import {Button} from "./Button.jsx";
 
 export const Footer = () => {
     const footerRef = useRef(null);
@@ -8,12 +8,14 @@ export const Footer = () => {
     useSmoothScroll(footerRef);
 
     return (
-        <footer className="footer" ref={footerRef}>
-            <Button href="#popup:pay">Отзывы</Button>
-            <Button href="#whoIsIt">Преподаватель</Button>
-            <Button href="#description">Программа</Button>
-            <Button href="#FAQ">FAQ</Button>
-            <Button style='last-btn' href="#header">Наверх</Button>
+        <footer >
+            <nav className="footer" ref={footerRef}>
+                <Button href="#popup:pay">Отзывы</Button>
+                <Button href="#whoIsIt">Преподаватель</Button>
+                <Button href="#description">Программа</Button>
+                <Button href="#FAQ">FAQ</Button>
+                <Button style='last-btn' href="#header">Наверх</Button>
+            </nav>
         </footer>
     );
 };
